662feada2c03ef654397a79c6d0430ef94c6e380
Qwasiomorph <boggdan56@gmail.com>
2023-08-08 10:43:56 +0300

    refactor filter

--- src/components/TasksFilter/TasksFilter.jsx
+++ src/components/TasksFilter/TasksFilter.jsx
@@ -1,16 +1,14 @@
 const TasksFilter = ({ filter, setFilter }) => {
+  const handleFilter = (event) => {
+    event.preventDefault();
+    setFilter(event.target.name);
+  };
+
   return (
     <ul className="filters">
       {['All', 'Active', 'Completed'].map((filt) => (
         <li key={filt}>
-          <button
-            type="button"
-            className={filter === filt ? 'selected' : ''}
-            onClick={(e) => {
-              e.preventDefault();
-              setFilter(filt);
-            }}
-          >
+          <button type="button" name={filt} className={filter === filt ? 'selected' : ''} onClick={handleFilter}>
             {filt}
           </button>
         </li>
